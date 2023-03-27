ff2029dd129c9a2fa10964af2309295bf9007c8d
XanderIRE <alexmrireland@gmail.com>
2023-03-27 21:59:15 +0100

    correct serial pin conn for trrs

---
+++ qmk/keyboards/olacc34/config.h
@@ -0,0 +1,35 @@
+// Copyright 2023 Alexander Ireland (@XanderIRE)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#include "config_common.h"
+
+#define USE_SERIAL
+#define SOFT_SERIAL_PIN D2
+
+/* key matrix size */
+#define MATRIX_ROWS 8
+#define MATRIX_COLS 5
+
+#define DIRECT_PINS {{F7, F6, F5, F4, D3}, \
+                     {D1, B1, B3, B2, B6}, \
+                     {D0, D4, C6, D7, E6}, \
+                     {NO_PIN, NO_PIN, NO_PIN, B4, B5}}
+#define DIRECT_PINS_RIGHT {{D3, F4, F5, F6, F7}, \
+                           {B6, B2, B3, B1, D1}, \
+                           {E6, D7, C6, D4, D0}, \
+                           {B5, B4, NO_PIN, NO_PIN, NO_PIN}}
+
+/* Top left key on left half */
+#define BOOTMAGIC_LITE_ROW    0
+#define BOOTMAGIC_LITE_COLUMN 0
+/* Top right key on right half */
+#define BOOTMAGIC_LITE_ROW_RIGHT    0
+#define BOOTMAGIC_LITE_COLUMN_RIGHT 4
+
+/* Mod-tap / Layer-tap Useability Settings */
+#define TAPPING_TERM 225
+#define IGNORE_MOD_TAP_INTERRUPT
+#define PERMISSIVE_HOLD
+#define RETRO_TAPPING_PER_KEY
