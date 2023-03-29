9006ecfb165becbb18c67eff2871f97201a972bd
Alexander Ireland <110393757+XanderIRE@users.noreply.github.com>
2023-03-29 21:07:10 +0100

    Fix reversed pins for right half

--- qmk/keyboards/olacc34/config.h
+++ qmk/keyboards/olacc34/config.h
@@ -16,10 +16,10 @@
                      {D1, B1, B3, B2, B6}, \
                      {D0, D4, C6, D7, E6}, \
                      {NO_PIN, NO_PIN, NO_PIN, B4, B5}}
-#define DIRECT_PINS_RIGHT {{D3, F4, F5, F6, F7}, \
-                           {B6, B2, B3, B1, D1}, \
-                           {E6, D7, C6, D4, D0}, \
-                           {B5, B4, NO_PIN, NO_PIN, NO_PIN}}
+#define DIRECT_PINS_RIGHT {{F7, F6, F5, F4, D3}, \
+                     {D1, B1, B3, B2, B6}, \
+                     {D0, D4, C6, D7, E6}, \
+                     {NO_PIN, NO_PIN, NO_PIN, B4, B5}}
 
 /* Top left key on left half */
 #define BOOTMAGIC_LITE_ROW    0
